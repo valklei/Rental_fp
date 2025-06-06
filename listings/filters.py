4fd630df85b3cd635ce8d52ec691a8c7acbf5d82
Valerii <valklei1964@gmail.com>
2025-06-06 17:03:05 +0200

    add filter by rooms_count

--- listings/filters.py
+++ listings/filters.py
@@ -7,15 +7,25 @@ class ListingFilter(django_filters.FilterSet):
     price_min = django_filters.NumberFilter(field_name="price", lookup_expr='gte')
     price_max = django_filters.NumberFilter(field_name="price", lookup_expr='lte')
     city = django_filters.CharFilter(field_name="location", lookup_expr='icontains')
+    rooms_count = django_filters.NumberFilter(field_name="rooms_count", lookup_expr='gte')
+    rooms_count_min = django_filters.NumberFilter(field_name="rooms_count", lookup_expr='gte')
+    rooms_count_max = django_filters.NumberFilter(field_name="rooms_count", lookup_expr='lte')
+
     class Meta:
         model = Listing
-        fields = ['price_min', 'price_max', 'location']
+        fields = ['price_min', 'price_max', 'city', 'rooms_count', 'rooms_count_min', 'rooms_count_max']
 
     def filter_queryset(self, queryset):
         data = self.form.cleaned_data
 
         price_min = data.get('price_min')
         price_max = data.get('price_max')
+        rooms_count_min = data.get('rooms_count_min')
+        rooms_count_max = data.get('rooms_count_max')
+
+        if rooms_count_min is not None and rooms_count_max is not None:
+            if rooms_count_min > rooms_count_max:
+                raise ValidationError("Минимальное количество комнат не может быть больше максимального.")
 
         if price_min is not None and price_max is not None:
             if price_min > price_max:
